9f8b69bb7f80eea669f55b111e1ce78c892ce6b8
tanvirewu <tanvirewu32@gmail.com>
2022-05-22 00:16:14 +0600

    structure/structing the files

--- src/components/RentalHomeDataTable.js
+++ src/components/RentalHomeDataTable.js
@@ -1,94 +1,81 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core";
-import EnhancedTable from "../modules/DataTable";
-import ProductCheckout from "../modules/ProductCheckout";
-import Data from "../data/Data.json";
-import Snackbar from "@material-ui/core/Snackbar";
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import EnhancedTable from '../modules/DataTable'
+import ProductCheckout from '../modules/ProductCheckout'
+import Data from '../data/Data.json'
+import Snackbar from '@material-ui/core/Snackbar'
 
-//to insert IDs on given Data
 function insertIDs(data) {
-  let temp = [];
-  data.map((item, index) => {
-    temp = [...temp, { ...item, id: index }];
-  });
-  //console.log(data);
-  return temp;
+	let temp = []
+	data.map((item, index) => {
+		temp = [...temp, { ...item, id: index }]
+		return item
+	})
+	return temp
 }
 
-// SetItem is mainly used for saving the data in localStorage
-
-// initDataFromLocalStorage funtion is mainly used for fetch data from localstorage if available,else set from json data 
 function initDataFromLocalStorage(data) {
-  if (!localStorage.getItem("dataList")) {
-    localStorage.setItem("dataList", JSON.stringify(insertIDs(data)));
-  }
-// Using JSON.stringify in the setItem is optional when saving string data to the storage.However, JSON.stringify is required if the value is a different data type, like an object or array.
-  const fetchedData = JSON.parse(localStorage.getItem("dataList"));
+	if (!localStorage.getItem('dataList')) {
+		localStorage.setItem('dataList', JSON.stringify(insertIDs(data)))
+	}
+	const fetchedData = JSON.parse(localStorage.getItem('dataList'))
 
-// Here the getItem() storage method to retrieve data from the local storage.  
-  return fetchedData;
+	return fetchedData
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "80%",
-  },
-  localStorageParagraph: {
-    textAlign: "left",
-    fontFamily: "monospace",
-    fontSize: 12,
-    textDecoration: "underline",
-    cursor: "pointer",
-    marginLeft: 10,
-  },
-}));
+	root: {
+		width: '80%',
+	},
+	localStorageParagraph: {
+		textAlign: 'left',
+		fontFamily: 'monospace',
+		fontSize: 12,
+		textDecoration: 'underline',
+		cursor: 'pointer',
+		marginLeft: 10,
+	},
+}))
+
 export default function RentalHomeDataTable() {
-  const classes = useStyles();
-  const [rows, setRows] = React.useState(initDataFromLocalStorage(Data));
-  const [selectedProduct, setSelectedProduct] = React.useState(null);
-  const [showNotification, setNotificationOpen] = React.useState(false);
-  
-  const resetLocalstorage = () => {
-    console.log("ok");
-    if (localStorage.getItem("dataList")) localStorage.removeItem("dataList");
-    setRows(initDataFromLocalStorage(Data));
-  };
-  const callBackForTable = (data) => {
-    setSelectedProduct(data);
-  };
+	const classes = useStyles()
+	const [rows, setRows] = React.useState(initDataFromLocalStorage(Data))
+	const [selectedProduct, setSelectedProduct] = React.useState(null)
+	const [showNotification, setNotificationOpen] = React.useState(false)
 
-  //callback to update product table after successfully booking or returning
-  const updateTable = () => {
-    const fetchedFromLocalStorage = JSON.parse(
-      localStorage.getItem("dataList")
-    );
+	const callBackForTable = (data) => {
+		setSelectedProduct(data)
+	}
 
-    setRows(fetchedFromLocalStorage);
-    setSelectedProduct(null);
-    setNotificationOpen(true);
-  };
+	const updateTable = () => {
+		const fetchedFromLocalStorage = JSON.parse(
+			localStorage.getItem('dataList')
+		)
 
-  return (
-    <div className={classes.root}>
-      <EnhancedTable
-        dataList={rows}
-        selectedProduct={selectedProduct}
-        callBackForTable={callBackForTable}
-      />
+		setRows(fetchedFromLocalStorage)
+		setSelectedProduct(null)
+		setNotificationOpen(true)
+	}
 
-      <ProductCheckout
-        dataList={rows}
-        selectedRow={selectedProduct}
-        updateTable={updateTable}
-      />
-
-      <Snackbar
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={showNotification}
-        onClose={() => setNotificationOpen(false)}
-        message="Rental Info Updated"
-        autoHideDuration={3000}
-      />
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<EnhancedTable
+				dataList={rows}
+				selectedProduct={selectedProduct}
+				callBackForTable={callBackForTable}
+			/>
+			<ProductCheckout
+				dataList={rows}
+				selectedRow={selectedProduct}
+				updateTable={updateTable}
+			/>
+			<Snackbar
+				anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+				open={showNotification}
+				onClose={() => setNotificationOpen(false)}
+				message='Rental Info Updated'
+				autoHideDuration={3000}
+			/>
+		</div>
+	)
 }
--- src/modules/DataTable/components/CustomToolbarDatePicker.js
+++ src/modules/DataTable/components/CustomToolbarDatePicker.js
@@ -1,35 +1,31 @@
-import React, { useState } from "react";
-import PickerToolbar from "@material-ui/pickers/_shared/PickerToolbar";
-import ToolbarButton from "@material-ui/pickers/_shared/ToolbarButton";
-import { makeStyles } from "@material-ui/core";
+import React from 'react'
+import PickerToolbar from '@material-ui/pickers/_shared/PickerToolbar'
+import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles({
-  toolbar: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    height: 15,
-  },
-});
+	toolbar: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'flex-start',
+		height: 15,
+	},
+})
 
 const CustomToolbar = function (props) {
-  const { date, isLandscape, openView, setOpenView, title } = props;
+	const { isLandscape, title } = props
+	const classes = useStyles()
 
-  const handleChangeViewClick = (view) => (e) => {
-    setOpenView(view);
-  };
+	return (
+		<PickerToolbar
+			className={classes.toolbar}
+			title={title}
+			isLandscape={isLandscape}
+		>
+			<h2 style={{ color: 'white', fontWeight: 300 }}>
+				{props.label} Date
+			</h2>
+		</PickerToolbar>
+	)
+}
 
-  const classes = useStyles();
-
-  return (
-    <PickerToolbar
-      className={classes.toolbar}
-      title={title}
-      isLandscape={isLandscape}
-    >
-      <h2 style={{ color: "white", fontWeight: 300 }}>{props.label} Date</h2>
-    </PickerToolbar>
-  );
-};
-
-export default CustomToolbar;
+export default CustomToolbar
